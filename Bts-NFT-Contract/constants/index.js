db6c335f231cb3483a2d280d9b1db27028571a57
iykethe1st <iykethe1st@outlook.com>
2022-05-29 13:02:51 +0100

    fifth commit

--- Bts-NFT-Contract/constants/index.js
+++ Bts-NFT-Contract/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x05fFC8E143B6ab32c291a3E204bA42865D6057d4";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://bts-collection.vercel.app/api/";
 
 module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
